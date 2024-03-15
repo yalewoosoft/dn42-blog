f557d92a10cb4f27b55cc1bde7937c409cbec94b
Yanxiu Wuwang <uofvo@student.kit.edu>
2024-03-15 15:19:06 +0000

    initial commit

---
+++ content/hello.md
@@ -0,0 +1,7 @@
+Title: Hello
+Date: 2023-03-14 13:00
+Category: Welcome
+
+This website is hosted on the DN42 network in AS4242421967.
+DN42 (https://dn42.eu) is a dynamic VPN that utilizes existing Internet technology to create an overlay network over the Internet.
+
---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+AUTHOR = 'SAKRIKO'
+SITENAME = 'AS4242421967-SAKRIKO'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'Europe/Berlin'
+
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+THEME = "eevee"
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("DN42", "https://dn42.eu"),
+)
+
+# Social widget
+SOCIAL = (
+        ("GitHub", "https://github.com/yalewoosoft"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
